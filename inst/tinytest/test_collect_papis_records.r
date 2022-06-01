d6509bb2bc258687ca1c6c16b09253d1c1ca9d2f
stas <stas@air.local>
2022-06-01 12:39:42 +0200

    Fix paths in tests

--- inst/tinytest/test_collect_papis_records.r
+++ inst/tinytest/test_collect_papis_records.r
@@ -1,7 +1,7 @@
 expect_equal(
   system.file("testdata", "papis", package = "papisr") |>
-  collect_papis_records("data" %in% info$tags)
-, list(list(path = "/Library/Frameworks/R.framework/Versions/4.1/Resources/library/papisr/testdata/papis/a", 
-    info = list(tags = c("data", "research"), url = "example.com", 
-        year = 2022L)), list(path = "/Library/Frameworks/R.framework/Versions/4.1/Resources/library/papisr/testdata/papis/c", 
-    info = list(tags = "data", year = 2222L))))
+  collect_papis_records("data" %in% info$tags) |>
+  lapply(`[[`, "info")
+  ## remove paths as in test environment it is different
+, list(list(tags = c("data", "research"), url = "example.com", 
+    year = 2022L), list(tags = "data", year = 2222L)))
--- inst/tinytest/test_tabulate_papis_records.r
+++ inst/tinytest/test_tabulate_papis_records.r
@@ -3,6 +3,7 @@ expect_equal(system.file("testdata", "papis", package = "papisr") |>
              tabulate_papis_records(year = info$year
                                   , url = info$url
                                   , tag = length(info$tags)
+                                    ## remove paths as in test environment it is different
                                   , use_path_as_row_names = FALSE)
 , structure(list(2022L, 1985L, 2222L, 2000L, "example.com", "uvt.nl", 
     NA, NA, 2L, 2L, 1L, 1L), .Dim = 4:3, .Dimnames = list(NULL, 
